281e8ef43db689ae544224e69d0ba851c6546ead
Arthur Apanovics <arthur.apanovics@gmail.com>
2023-02-22 12:10:59 +1300

    Refactor existing BulkPrice ctor tests

---
+++ TerminalUnitTests/Models/BulkPriceTests/ConstructorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Terminal.Models;
+using Xunit;
+
+namespace TerminalUnitTests.Models.BulkPriceTests;
+
+public class ConstructorTests
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-3)]
+    public void ThrowsWhenThresholdValueNotValid(int threshold)
+    {
+        // Arrange
+        var actual = () => new BulkPrice(threshold, price: 3m);
+
+        // Act/Assert
+        actual.Should().ThrowExactly<ArgumentException>();
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-3)]
+    public void ThrowsWhenBulkPriceValueNotValid(decimal price)
+    {
+        // Arrange
+        var actual = () => new BulkPrice(threshold: 3, price);
+
+        // Act/Assert
+        actual.Should().ThrowExactly<ArgumentException>();
+    }
+}
